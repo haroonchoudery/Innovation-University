b730d31bf50468692fedd6c4ae1a7cc7141a03b8
Truong Vo <truong.vo@seadev.com.vn>
2014-08-06 07:36:53 +0700

    Add some fields and change the slide

--- sites/all/themes/magazeen_lite/templates/page.tpl.php
+++ sites/all/themes/magazeen_lite/templates/page.tpl.php
@@ -37,24 +37,9 @@
 
 <!-- ______________________ SLIDESHOW _______________________ -->
 <?php if (drupal_is_front_page()): ?>
-<div id="slideshow"><!-- slideshow -->
+<div id="slideshow"><!-- static image -->
   <div class="slideshow container clearfix">
-    <div class="force-previous"><a href="#">Previous</a></div>
-    <div class="main_view">
-      <div class="window">
-        <div class="image_reel">
-            <a href="#"><img src="<?php print base_path() . drupal_get_path('theme', 'magazeenlite')?>/images/slideshow/slide3.jpg" alt="" /></a>
-            <a href="#"><img src="<?php print base_path() . drupal_get_path('theme', 'magazeenlite')?>/images/slideshow/slide1.jpg" alt="" /></a>
-            <a href="#"><img src="<?php print base_path() . drupal_get_path('theme', 'magazeenlite')?>/images/slideshow/slide2.jpg" alt="" /></a>
-        </div>
-      </div>
-      <div class="paging">
-        <a href="#" rel="1">1</a>
-        <a href="#" rel="2">2</a>
-        <a href="#" rel="3">3</a>
-      </div>
-    </div>
-    <div class="force-next"><a href="#">Next</a></div>
+    <a href="#"><img src="<?php print base_path() . drupal_get_path('theme', 'magazeenlite')?>/images/slide1.png" alt="" /></a>
   </div> <!-- /slideshow-container -->
 </div> <!-- /slideshow -->
 <?php endif;?>
